7bbfcf9998a1845a58682d01c9ea418dd1d4daa6
unknown <lidanyang1111@gmail.com>
2022-01-27 16:17:47 -0800

    v5

--- api/jokes.test.js
+++ api/jokes.test.js
@@ -3,6 +3,9 @@ const db = require("../data/db-config")
 const server = require("./server")
 const Joke = require("./jokes/jokesModel")
 
+const joke1 = { joke:"1", punchline:"1" }
+const joke2 = { joke:"2", punchline:"2" }
+
 beforeAll(async () => {
     await db.migrate.rollback()
     await db.migrate.latest()
@@ -27,6 +30,30 @@ describe("Jokes model funcitons", () => {
             await Joke.createJoke(joke1)
             jokes = await db("jokes")
             expect(jokes).toHaveLength(1)
+
+            await Joke.createJoke(joke2)
+            jokes = await db("jokes")
+            expect(jokes).toHaveLength(2)
+        })
+        it("inserted joke and punchline", async () => {
+            const joke = await Joke.createJoke(joke1)
+            expect(joke).toMatchObject({joke_id:1, ...joke})
+        })
+    })
+
+    describe("[DELETE] / - deletes joke", () => {
+        it("removes joke from db", async () => {
+            const [ joke_id ] = await db("jokes").insert(joke1)
+            let joke = await db("jokes").where({joke_id}).first()
+            expect(joke).toBeTruthy()
+            await request(server).delete("/jokes/" + joke_id)
+            joke = await db("jokes").where({joke_id}).first()
+            expect(joke).toBeFalsy()
+        })
+        it("respond with the deleted joke", async () => {
+            await db("jokes").insert(joke1)
+            let joke = await request(server).delete("/jokes/1")
+            expect(joke.body).toMatchObject(joke1)
         })
     })
 })
